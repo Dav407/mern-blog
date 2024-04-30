1f8dab28e378675bcad0a98392c0e14dec77578e
etim david <etimdavid407@gmail.com>
2024-04-29 21:00:45 -0700

    created pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Project from './pages/Project'
+import Dashboardd from './pages/Dashboard'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-      <h1  className="text-3xl  text-red-500">app</h1>
-      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/project' element={<Project/>}/>
+        <Route path='/dashboard' element={<Dashboardd/>}/>
+      </Routes>
+      </BrowserRouter>
     )
   }
 }
